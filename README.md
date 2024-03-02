f412cf2518ac090f6c354c24fbf003742e9c7137
TuvalSimha <tuval.simha@gmail.com>
2024-03-02 18:50:01 +0200

    Fix some

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # happy-animation
+
+## Description
+
+happy-animation is an open-source React library that provides cool and happy animations, such as confetti. Currently, it includes a ConfettiButton component for creating confetti effects with various customization options.
+
+## Installation
+
+You can install the package using npm:
+
+```
+npm install happy-animation
+```
+
+or using yarn:
+
+```
+yarn add happy-animation
+```
+
+## Usage
+
+### Confetti Component
+
+```
+import React from "react";
+import { ConfettiButton } from "happy-animation";
+
+const YourComponent = () => {
+  return (
+    <div>
+      {/* Customized ConfettiButton */}
+      <ConfettiButton
+        shapes={["square", "triangle", "circle", "star", "heart"]}
+        animationDuration={4}
+        buttonDesign={<YourCustomButtonComponent />}
+        colorDigit="ABCDEF1234567890"
+        confettiQuantity={50}
+      />
+
+      {/* Default ConfettiButton */}
+      <ConfettiButton />
+    </div>
+  );
+};
+```
+
+#### Props
+
+- shapes: Array of shapes for confetti (default: ["square", "triangle", "circle", "star", "heart"]).
+- animationDuration: Duration of the confetti animation in seconds (default: 4).
+- buttonDesign: Custom design for the confetti button.
+- colorDigit: Custom color digit for confetti (default: "ABCDEF1234567890").
+- confettiQuantity: Number of confetti pieces to generate (default: 50).
+
+## Example
+
+Visit our demo page to see happy-animation in action!
+
+## Contributing
+
+We welcome contributions! If you find any issues or have ideas for improvements, feel free to open an issue or submit a pull request.
--- src/components/confetti/confetti.tsx
+++ src/components/confetti/confetti.tsx
@@ -4,67 +4,49 @@ import { ConfettiProps } from "./types";
 
 const ConfettiButton: React.FC<ConfettiProps> = ({
   shapes,
-  duration,
+  animationDuration,
   buttonDesign,
   colorDigit,
   confettiQuantity,
 }) => {
-  const SHAPES = shapes || ["square", "triangle", "circle"];
-  const COLOR_DIGIT = "ABCDEF1234567890" || colorDigit;
-  const generateRandomColor = () => {
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += COLOR_DIGIT[Math.floor(Math.random() * COLOR_DIGIT.length)];
-    }
-    return color;
+  const SHAPES = shapes || ["square", "triangle", "circle", "star", "heart"];
+  const generateRandomColor = (color = "ABCDEF1234567890" ?? colorDigit) => {
+    return (
+      "#" +
+      Array.from(
+        { length: 6 },
+        () => color[Math.floor(Math.random() * color.length)]
+      ).join("")
+    );
   };
 
   const generateConfetti = () => {
-    const container = containerRef.current as unknown as HTMLDivElement;
-    if (container) {
-      if (confettiQuantity) {
-        for (let i = 0; i < confettiQuantity; i++) {
-          const confetti = document.createElement("div");
-          const positionX = Math.random() * window.innerWidth;
-          const positionY = Math.random() * window.innerHeight;
-          const rotation = Math.random() * 360;
-          const size = Math.floor(Math.random() * (20 - 5 + 1)) + 5; // Set confetti styles
-          confetti.style.left = `${positionX}px`;
-          confetti.style.top = `${positionY}px`;
-          confetti.style.transform = `rotate(${rotation}deg)`;
-          confetti.className =
-            "confetti " + SHAPES[Math.floor(Math.random() * 3)];
-          confetti.style.width = `${size}px`;
-          confetti.style.height = `${size}px`;
-          confetti.style.backgroundColor = generateRandomColor(); // Append confetti to the container
-          container.appendChild(confetti);
-          // Remove confetti element after animation duration (4 seconds)
-          setTimeout(() => {
-            container.removeChild(confetti);
-          }, 4000);
-        }
-      } else {
-        for (let i = 0; i < 50; i++) {
-          const confetti = document.createElement("div");
-          const positionX = Math.random() * window.innerWidth;
-          const positionY = Math.random() * window.innerHeight;
-          const rotation = Math.random() * 360;
-          const size = Math.floor(Math.random() * (20 - 5 + 1)) + 5; // Set confetti styles
-          confetti.style.left = `${positionX}px`;
-          confetti.style.top = `${positionY}px`;
-          confetti.style.transform = `rotate(${rotation}deg)`;
-          confetti.className =
-            "confetti " + SHAPES[Math.floor(Math.random() * 3)];
-          confetti.style.width = `${size}px`;
-          confetti.style.height = `${size}px`;
-          confetti.style.backgroundColor = generateRandomColor(); // Append confetti to the container
-          container.appendChild(confetti);
-          // Remove confetti element after animation duration (4 seconds)
-          setTimeout(() => {
-            container.removeChild(confetti);
-          }, 4000);
-        }
-      }
+    const container = containerRef.current as unknown as HTMLElement;
+
+    if (!container) return;
+
+    const numberOfConfetti = confettiQuantity || 50;
+
+    for (let i = 0; i < numberOfConfetti; i++) {
+      const confetti = document.createElement("div");
+      const positionX = Math.random() * window.innerWidth;
+      const positionY = Math.random() * window.innerHeight;
+      const rotation = Math.random() * 360;
+      const size = Math.floor(Math.random() * (20 - 5 + 1)) + 5;
+
+      confetti.style.left = `${positionX}px`;
+      confetti.style.top = `${positionY}px`;
+      confetti.style.transform = `rotate(${rotation}deg)`;
+      confetti.className = "confetti " + SHAPES[Math.floor(Math.random() * 3)];
+      confetti.style.width = `${size}px`;
+      confetti.style.height = `${size}px`;
+      confetti.style.backgroundColor = generateRandomColor();
+
+      container.appendChild(confetti);
+
+      setTimeout(() => {
+        container.removeChild(confetti);
+      }, animationDuration * 1000 || 4000);
     }
   };
 
@@ -80,13 +62,16 @@ const ConfettiButton: React.FC<ConfettiProps> = ({
     setConfettiActive(true);
     setTimeout(() => {
       setConfettiActive(false);
-    }, duration || 4000);
+    }, animationDuration * 1000 || 4000);
   };
 
   return (
     <div>
       {buttonDesign ? (
-        <button className="confetti-button" onClick={handleClick}>
+        <button
+          className={`confetti-button animate-fall-${animationDuration}s`}
+          onClick={handleClick}
+        >
           {buttonDesign}
         </button>
       ) : (
@@ -98,7 +83,7 @@ const ConfettiButton: React.FC<ConfettiProps> = ({
         className="fixed top-0 left-0 w-full h-full pointer-events-none"
         ref={containerRef}
         id="confetti-container"
-      ></div>
+      />
     </div>
   );
 };
